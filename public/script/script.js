9ccfa1b710a5b069a38da816918032de26acfb78
Zach Selindh <zachselindh@gmail.com>
2019-04-25 21:00:39 -0500

    Fixed dropdown, added center styling.

--- public/script/script.js
+++ public/script/script.js
@@ -8,8 +8,14 @@ $(".dropdown-menu li").click(function() {
   if (newsBias === "right") {
     $("#news-drop").addClass("btn-danger");
     $("#news-drop").removeClass("btn-primary");
+    $("#news-drop").removeClass("btn-info");
   } else if (newsBias === "left") {
     $("#news-drop").removeClass("btn-danger");
     $("#news-drop").addClass("btn-primary");
+    $("#news-drop").removeClass("btn-info");
+  } else if (newsBias === "center") {
+    $("#news-drop").removeClass("btn-danger");
+    $("#news-drop").removeClass("btn-primary");
+    $("#news-drop").addClass("btn-info");
   }
 });
